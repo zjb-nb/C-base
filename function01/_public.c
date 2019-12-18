c50b1339e8025fa7e883d362d7aef1615ceabd1e
1715776882@qq.com <1715776882@qq.com>
2019-12-18 11:36:03 +0800

    自定义公共函数

---
+++ function01/_public.c
@@ -0,0 +1,9 @@
+#include "_public.h"
+
+int max(int i1,int i2)
+{
+int res=0;
+ res = i1>i2 ?i1: i2;
+ return res; 
+
+}
---
+++ function01/book.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "_public.h"
+
+int main ()
+{
+printf("%d",max(30,50));
+return 0;
+}
+
